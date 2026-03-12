3324820496e0a1da327d8ce2b650ee297b413881
Simon Li <spli@dundee.ac.uk>
2026-03-12 12:36:04 +0000

    Remove proxy and ecs cluster for now

--- services.tf
+++ services.tf
@@ -22,30 +22,10 @@ module "dnsresolver" {
 
   static-ttl = 3600
   static = [
-    # ECS Aliases
-    ["proxy", "CNAME", "squid-proxy.${var.dns_domain}"],
+    # # ECS Aliases
+    # ["proxy", "CNAME", "squid-proxy.${var.dns_domain}"],
   ]
 
   # For now allow all since K8TRE is fetching external images and code
   allowed_domains = ["*."]
-}
-
-
-module "cluster" {
-  source           = "./ecs-cluster"
-  name             = "squid-proxy"
-  vpc_id           = module.vpc.vpc_id
-  vpc_r53_id       = module.vpc.vpc_id
-  subnets          = slice(module.vpc.public_subnets, 0, 2)
-  discovery_domain = "ecs.${var.dns_domain}"
-}
-
-module "squid" {
-  source      = "./proxy"
-  ecs_cluster = module.cluster.ecs_cluster
-  kms_key     = module.cluster.kms_arn
-
-  vpc_id           = module.vpc.vpc_id
-  public_subnets   = slice(module.vpc.public_subnets, 0, 2)
-  ecs_discovery_id = module.cluster.discovery_id
 }
